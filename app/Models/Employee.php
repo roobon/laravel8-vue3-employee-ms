4c6978420aa9ba81e88e4f6f841da106e1784d7d
naymur92 <naymurrahman@yahoo.com>
2023-04-04 12:23:09 +0600

    adminLTE template added in backend, fix dashboard page and login page

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -7,26 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    use HasFactory;
-    protected $fillable = ['firstname', 'lastname', 'address', 'country_id', 'state_id', 'city_id', 'department_id'];
+  use HasFactory;
+  protected $fillable = ['firstname', 'lastname', 'address', 'country_id', 'state_id', 'city_id', 'department_id'];
 
-    public function country()
-    {
-        return $this->belongsTo(related: Country::class, foreignKey: 'country_id');
-    }
+  public function country()
+  {
+    return $this->belongsTo(related: Country::class, foreignKey: 'country_id');
+  }
 
-    public function state()
-    {
-        return $this->belongsTo(related: State::class, foreignKey: 'state_id');
-    }
+  public function state()
+  {
+    return $this->belongsTo(related: State::class, foreignKey: 'state_id');
+  }
 
-    public function city()
-    {
-        return $this->belongsTo(related: City::class, foreignKey: 'city_id');
-    }
+  public function city()
+  {
+    return $this->belongsTo(related: City::class, foreignKey: 'city_id');
+  }
 
-    public function department()
-    {
-        return $this->belongsTo(related: Department::class, foreignKey: 'department_id');
-    }
+  public function department()
+  {
+    return $this->belongsTo(related: Department::class, foreignKey: 'department_id');
+  }
 }
